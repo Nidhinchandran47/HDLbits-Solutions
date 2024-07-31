6a9b3863066e92d1e0b4d3e456d6d8afa90b6b44
Nidhin Chandran <nidhinchandran470@gmail.com>
2024-07-31 19:42:19 +0530

    31.7.24.1

---
+++ Circuits/Sequential Logic/FSM/Simple FSM 1 (async reset)/readme.md
@@ -0,0 +1,9 @@
+# Simple FSM 1
+
+This is a Moore state machine with two states, one input, and one output. Implement this state machine. Notice that the reset state is B.
+
+![alt text](image.png)
+
+**Solution -> [FSM Design](solution_verilog.v)**
+
+Yes, there are ways to do this other than writing an FSM. This is a TFF with the T input inverted.
---
+++ Circuits/Sequential Logic/FSM/Simple FSM 1 (async reset)/solution_verilog.v
@@ -0,0 +1,39 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
+
+    parameter A=0, B=1; 
+    reg state, next_state;
+
+    always @(*) begin    // This is a combinational always block
+        // State transition logic
+        case (state)
+            A : begin
+                if(in)
+                    next_state <= A;
+                else
+                    next_state <= B;
+            end
+            B : begin
+                if(in)
+                    next_state <= B;
+                else
+                    next_state <= A;
+            end
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin    // This is a sequential always block
+        // State flip-flops with asynchronous reset
+        if(areset)
+            state <= B;
+        else
+            state <= next_state;
+    end
+
+    // Output logic
+            assign out = (state == B);
+
+endmodule
